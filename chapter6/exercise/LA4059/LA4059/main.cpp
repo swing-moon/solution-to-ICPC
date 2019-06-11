9229959a322a40dd4153b7ec210dbb7735dc94f8
fogsail <okayczm@sina.com>
2019-06-11 20:37:50 +0800

    Finished data-structures chapter5, chapter6

---
+++ chapter6/exercise/LA4059/LA4059/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LA4059
+//
+//  Created by zhangmin chen on 2019/5/27.
+//  Copyright © 2019 zhangmin chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
